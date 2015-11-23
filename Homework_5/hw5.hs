4499a26ff5512cc76aef936b1617fcfc7979eaa5
Bryan-Burkett <crazydude008@gmail.com>
2015-11-22 19:53:39 -0600

    yup

--- Homework_5/hw5.hs
+++ Homework_5/hw5.hs
@@ -6,7 +6,6 @@ import Control.Concurrent
 import Data.IORef
 
 main = do
-        id <- newTVarIO (0::Int)
         
         chopstick0 <- newTVarIO (1::Int) 
         chopstick1 <- newTVarIO (1::Int) 
@@ -17,31 +16,28 @@ main = do
 
         let loop phil_id = do
             --phil_id <- readTVarIO my_id
-            -- atomically $ take_chopstick (phil_id -1) chops 
-            -- atomically $ take_chopstick phil_id chops
-            -- eat phil_id
-            eat 1
-            -- atomically $ release_chopstick (phil_id - 1) chops
-            -- atomically $ release_chopstick phil_id chops
+            atomically $ take_chopstick (phil_id -1) chops 
+            atomically $ take_chopstick phil_id chops
+            eat phil_id
+            atomically $ release_chopstick (phil_id - 1) chops
+            atomically $ release_chopstick phil_id chops
             loop phil_id
-
-        let takeID = do
-            my_id <- readTVarIO id
-            
-            atomically $ writeTVar id $ my_id + 1
-            loop my_id
-        
-        forkIO $ takeID 
-        forkIO $ takeID
-        forkIO $ takeID
-        forkIO $ takeID
-        forkIO $ takeID
+ 
+        forkIO $ loop 1 
+        forkIO $ loop 2
+        forkIO $ loop 3
+        forkIO $ loop 4
+        forkIO $ loop 5
 
 -- take_both_chopsticks :: Int -> [TVar Int] -> STM ()
 
 take_chopstick :: Int -> [TVar Int] -> STM ()
-take_chopstick (-1) chops = writeTVar (chops !! 4) 0
-take_chopstick i chops = writeTVar (chops !! i) 0  
+take_chopstick i chops = do
+  if i == 0
+    then 
+  chopup <- readTVarIO (chops !! i) 
+  if (chopup == 1) 
+    then writeTVar chopup 
 
 release_chopstick :: Int -> [TVar Int] -> STM ()
 release_chopstick (-1) chops = writeTVar (chops !! 4) 1
