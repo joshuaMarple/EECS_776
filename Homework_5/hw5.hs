7376e20dd4a1f2e5ce1448ee45ede252727f2e40
Bryan-Burkett <crazydude008@gmail.com>
2015-11-22 18:54:23 -0600

    gettin there

--- Homework_5/hw5.hs
+++ Homework_5/hw5.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE GADTs, KindSignatures #-}
+
 import Control.Concurrent.STM
 import Control.Concurrent
 
@@ -10,38 +12,39 @@ main = do
         chopstick1 <- newTVarIO 1 
         chopstick2 <- newTVarIO 1 
         chopstick3 <- newTVarIO 1 
-        chopstick4 <- newTVarIO 1 
+        chopstick4 <- newTVarIO 1
+        chops <- [chopstick0,chopstick1, chopstick2,chopstick3,chopstick4]
 
-        let loop = do
-            atomically $ take_left_chopstick
-            atomically $ take_right_chopstick
-            eat
-            atomically $ release_left_chopstick
-            atomically $ release_right_chopstick
-            loop
+        let loop my_id = do
+            atomically $ take_left_chopstick ((readTVar my_id) -1) chops 
+            atomically $ take_right_chopstick (readTVar my_id) chops
+            eat my_id
+            atomically $ release_left_chopstick ((readTVar my_id) - 1) chops
+            atomically $ release_right_chopstick (readTVar my_id) chops
+            loop my_id
 
         let takeID = do
             my_id <- atomically (readTVar id)
             writeTVar id $ (atomically (readTVar id)) + 1
+            loop my_id
         
-        forkIO $ takeID $ loop
-        forkIO $ takeID $ loop
-        forkIO $ takeID $ loop
-        forkIO $ takeID $ loop
-        forkIO $ takeID $ loop
+        forkIO $ takeID 
+        forkIO $ takeID
+        forkIO $ takeID
+        forkIO $ takeID
+        forkIO $ takeID
 
-take_left_chopstick :: TVar -> Int -> IO ()
-take_left_chopstick a i = print ("pbfff") 
+take_left_chopstick :: Int -> [TVar Int] -> STM ()
+take_left_chopstick i chops = writeTVar (chops !! i) 0  
 
-take_right_chopstick :: TVar -> Int ->IO ()
-take_right_chopstick a i = print ("fffbp")
+take_right_chopstick :: Int -> [TVar Int] -> STM ()
+take_right_chopstick i chops = writeTVar (chops !! i) 0  
 
+release_left_chopstick :: Int -> [TVar Int] -> STM ()
+release_left_chopstick i chops = writeTVar (chops !! i) 1
 
-release_left_chopstick :: TVar -> Int ->IO ()
-release_left_chopstick a i = print ("fffbp")
-
-release_right_chopstick :: TVar -> Int ->IO ()
-release_right_chopstick a i = print ("fffbp")
+release_right_chopstick :: Int -> [TVar Int] -> STM ()
+release_right_chopstick i chops = writeTVar (chops !! i) 1
 
 eat :: Int -> IO ()
 eat id = print ("I'm philosopher ", id, " and I'm eating now.")
